da094e1f2105e733b8e07828317eb1c27800a360
luckybard <bartosz.mucha@outlook.com>
2023-10-11 23:07:41 +0200

    PAD_01

--- PAD_01/main.py
+++ PAD_01/main.py
@@ -1,28 +1,33 @@
+#     Ćwiczenie 1
 
-def check_range(valueToFind,start,end):
-    i = start
-    while i <= end:
-        if i == valueToFind:
-            print(str(valueToFind) + " jest między " + str(start) + " a " + str(end))
+def check_range(x, y, z):
+    i = y
+    while i <= z:
+        if i == x:
+            print(str(x) + " jest między " + str(y) + " a " + str(z))
             break
-        elif i == end:
-            print(str(valueToFind) + " nie jest między " + str(start) + " a " + str(end))
+        elif i == z:
+            print(str(x) + " nie jest między " + str(y) + " a " + str(z))
         i += 1
 
 check_range(20, 9, 228)
 check_range(7, 2, 5)
-def bool_range(valueToFind,start,end):
-    i = start
-    while i <= end:
-        if i == valueToFind:
+
+#     Ćwiczenie 2
+
+def bool_range(x, y, z):
+    i = y
+    while i <= z:
+        if i == x:
                 return True
-        elif i == end:
+        elif i == z:
             return False
         i += 1
 
 print("Czy podana wartość występuje w zbiorze? Wynik:" + str(bool_range(7, 5, 20)))
 print("Czy podane wartość występuje w zbiorze? Wynik:" + str(bool_range(67, 22, 25)))
 
+#     Ćwiczenie 3
 def unique_list(input_list):
     unique_elements = []
     for element in input_list:
@@ -32,10 +37,14 @@ def unique_list(input_list):
 
 unique_list([1,3,5,6,4,3,2,3,3,4,3,4,5,6,6,4,3,2,12,3,5,63,4,5,3,3,2])
 
+#     Ćwiczenie 4
+
 def objętość_kuli(promień):
         pi = 3.14
         objętość = (4/3) * pi * (promień**3)
         print("Objętość kuli wynosi" +str(round(objętość, 2)))
+
+#     Ćwiczenie 5
 def num_fact(n):
         if n == 0 or n == 1:
             return 1
